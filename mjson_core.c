c09a2c3a850f2189839c83df1c3b16af7f17a70b
Lelouch <LelouchHe@gmail.com>
2014-02-22 12:35:47 +0800

    add double set/get
    issue:
    0. comparing double is hard, so need to work that out
    1. codes are similar, find a way to reduce that
    2. 'set' should change the value, or create new value?

--- mjson_core.c
+++ mjson_core.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <float.h>
 #include <assert.h>
 
 #include "util/ref_str.h"
@@ -16,6 +17,15 @@ static void set_error(mjson_error_t *pe, int stat) {
     if (pe != NULL) {
         pe->stat = stat;
     }
+}
+
+/*
+ * 错误的实现
+ * 看来double的比较很复杂,考虑专门新建一个文件描述各种比较
+ * 要不就放到parser里?
+ */
+static int is_equal_double(double a, double b) {
+    return 0;
 }
 
 /*
@@ -53,7 +63,7 @@ int mj_int_get_error(mjson_t *mj, mjson_error_t *pe) {
     }
 
     if (mi->h.text == NULL) {
-        // 初始化时,text为NULL,默认输出0
+        /* 初始化时,text为NULL,默认输出0 */
         mi->i = 0;
     } else if (mi->i == 0) {
         if (!mi->h.is_str) {
@@ -66,12 +76,14 @@ int mj_int_get_error(mjson_t *mj, mjson_error_t *pe) {
             }
             strncpy(text, d.str + d.begin, len);
             text[len] = '\0';
+
             rs_fini(mi->h.text);
+
             mi->h.text = (ref_str_t *)text;
             mi->h.is_str = 1;
         }
 
-        mi->i = atoi((const char *)mi->h.text);
+        mi->i = strtol((const char *)mi->h.text, NULL, 10);
     }
 
     return mi->i;
@@ -88,9 +100,13 @@ void mj_int_set_error(mjson_t *mj, int value, mjson_error_t *pe) {
         set_error(pe, MJSONE_TYPE_ERROR);
         return;
     }
+    if (mi->i == value) {
+        set_error(pe, MJSONE_OK);
+        return;
+    }
 
-    char str[128];
-    size_t len = snprintf(str, 127, "%d", value);
+    char str[32];
+    size_t len = snprintf(str, 31, "%d", value);
     char *text = (char *)malloc((len + 1) * sizeof (char));
     if (text == NULL) {
         set_error(pe, MJSONE_MEM);
@@ -109,13 +125,89 @@ void mj_int_set_error(mjson_t *mj, int value, mjson_error_t *pe) {
     }
     mi->h.text = (ref_str_t *)text;
     mi->h.is_str = 1;
+    mi->h.is_dirty = 1;
+
+    set_error(pe, MJSONE_OK);
 }
 
 double mj_double_get_error(mjson_t *mj, mjson_error_t *pe) {
-    return 0;
+    if (mj == NULL) {
+        set_error(pe, MJSONE_NULL);
+        return 0.0;
+    }
+
+    TO_TYPE(mj, mjson_double_t, mi);
+    if (mi->h.type != MJSON_DOUBLE) {
+        set_error(pe, MJSONE_TYPE_ERROR);
+        return 0.0;
+    }
+
+    if (mi->h.text == NULL) {
+        /* 初始化时,text为NULL,默认输出0 */
+        mi->d = 0.0;
+    } else if (is_equal_double(mi->d, 0.0)) {
+        if (!mi->h.is_str) {
+            ref_str_data_t d = rs_get(mi->h.text);
+            size_t len = d.end - d.begin;
+            char *text = (char *)malloc((len + 1) * sizeof (char));
+            if (text == NULL) {
+                set_error(pe, MJSONE_MEM);
+                return 0;
+            }
+            strncpy(text, d.str + d.begin, len);
+            text[len] = '\0';
+
+            rs_fini(mi->h.text);
+
+            mi->h.text = (ref_str_t *)text;
+            mi->h.is_str = 1;
+        }
+
+        mi->d = strtod((const char *)mi->h.text, NULL);
+    }
+
+    return mi->d;
 }
 
 void mj_double_set_error(mjson_t *mj, double value, mjson_error_t *pe) {
+    if (mj == NULL) {
+        set_error(pe, MJSONE_NULL);
+        return;
+    }
+
+    TO_TYPE(mj, mjson_double_t, mi);
+    if (mi->h.type != MJSON_DOUBLE) {
+        set_error(pe, MJSONE_TYPE_ERROR);
+        return;
+    }
+    if (is_equal_double(mi->d, value)) {
+        set_error(pe, MJSONE_OK);
+        return;
+    }
+
+    char str[64];
+    size_t len = snprintf(str, 63, "%g", value);
+    char *text = (char *)malloc((len + 1) * sizeof (char));
+    if (text == NULL) {
+        set_error(pe, MJSONE_MEM);
+        return;
+    }
+    strncpy(text, str, len);
+    text[len] = '\0';
+
+    mi->d = value;
+    if (mi->h.text != NULL) {
+        if (mi->h.is_str) {
+            free((char *)mi->h.text);
+        } else {
+            rs_fini(mi->h.text);
+        }
+    }
+    mi->h.text = (ref_str_t *)text;
+    mi->h.is_str = 1;
+    mi->h.is_dirty = 1;
+
+    set_error(pe, MJSONE_OK);
 }
 
 /*
@@ -160,6 +252,8 @@ void mj_bool_set_error(mjson_t *mj, int value, mjson_error_t *pe) {
     } else {
         rp_reset(rp, mjson_false_ini(), (rp_fini_fun)mjson_false_fini);
     }
+    mb->h.is_dirty = 1;
+
     set_error(pe, MJSONE_OK);
 }
 
--- unittest/mjson_core_test.c
+++ unittest/mjson_core_test.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
+#include <limits.h>
+#include <float.h>
 
 #include "mjson_core.h"
 
 int main() {
-    mjson_t *mj = mj_ini(MJSON_FALSE);
+    mjson_t *mj = NULL;
+
+    mj = mj_ini(MJSON_FALSE);
     printf("false: %d\n", mj_bool_get_error(mj, NULL));
     mj_bool_set_error(mj, 1, NULL);
     printf("true : %d\n", mj_bool_get_error(mj, NULL));
     mj_fini(mj);
 
-    mjson_t *nmj = mj_ini(MJSON_INTEGER);
-    printf("%d\n", mj_int_get_error(nmj, NULL));
-    mj_int_set_error(nmj, 0, NULL);
-    printf("%d\n", mj_int_get_error(nmj, NULL));
-    mj_fini(nmj);
+    mj = mj_ini(MJSON_INTEGER);
+    printf("%d\n", mj_int_get_error(mj, NULL));
+    mj_int_set_error(mj, INT_MAX, NULL);
+    printf("%d\n", mj_int_get_error(mj, NULL));
+    mj_fini(mj);
+
+    mj = mj_ini(MJSON_DOUBLE);
+    printf("%g\n", mj_double_get_error(mj, NULL));
+    mj_double_set_error(mj, DBL_MIN, NULL);
+    printf("%g\n", mj_double_get_error(mj, NULL));
+    mj_fini(mj);
+
     return 0;
 }
